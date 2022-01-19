f6be22fb692552b7f9c7af0fb439ddad89e7c8a1
Reetesh Sudhakar <reesud6187@gmail.com>
2022-01-19 14:57:13 -0500

    modify particle bubble colors

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,13 +8,13 @@ particlesJS("particles-js", {
       }
     },
     "color": {
-      "value": "#243b4a"
+      "value": "#ee6258"
     },
     "shape": {
       "type": "circle",
       "stroke": {
         "width": 0,
-        "color": "#243b4a"
+        "color": "#ee6258"
       },
       "polygon": {
         "nb_sides": 5
